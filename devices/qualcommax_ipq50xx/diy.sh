049ffc4db4a830e95b612a4fe299ff4e35c89908
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-09-02 04:27:27 +0800

    Update diy.sh

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -8,13 +8,7 @@ rm -rf package/boot package/kernel package/firmware/ipq-wifi target/linux/qualco
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/kernel package/boot package/qca package/firmware/ipq-wifi
 
-wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/include/kernel-version.mk -P include/
-wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/include/target.mk -P include/
-
-sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-advancedplus luci-app-firewall luci-app-package-manager luci-app-upnp luci-app-syscontrol luci-proto-wireguard \
-luci-app-wizard luci-base luci-compat luci-lib-ipkg luci-lib-fs luci-app-log-viewer \
-coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan luci-app-filemanager /" include/target.mk
-sed -i "s/procd-ujail//" include/target.mk
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.12 -P include/
 
 sed -i "s/wpad-openssl/wpad-mbedtls/" target/linux/qualcommax/Makefile
 
