e1287aeff03eec4e21785215cbeae8de9d9a196c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-09-02 17:16:38 +0800

    Update diy.sh

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -6,9 +6,12 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 rm -rf package/boot package/kernel package/firmware/ipq-wifi target/linux/qualcommax target/linux/generic
 
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/kernel package/boot package/qca package/firmware/ipq-wifi
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/qca package/firmware/ipq-wifi
+git_clone_path main https://github.com/openwrt/openwrt package/kernel
 
 wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.12 -P include/
+
+wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.10/package/kernel/bpf-headers/Makefile -P package/kernel/bpf-headers/
 
 sed -i "s/wpad-openssl/wpad-mbedtls/" target/linux/qualcommax/Makefile
 
