08207e1ffb76e9f8d476c9e3fa6837270a48a6bf
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-12-23 20:08:32 +0800

    update

--- devices/amlogic_mesongx/diy.sh
+++ devices/amlogic_mesongx/diy.sh
@@ -5,9 +5,6 @@ shopt -s extglob
 rm -rf package/kernel
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/backport-6.1 target/linux/generic/hack-6.1 target/linux/generic/pending-6.1 target/linux/amlogic package/boot/uboot-amlogic package/kernel
 
-rm -rf package/kernel/bpf-headers
-git_clone_path openwrt-25.12 https://github.com/openwrt/openwrt package/kernel/bpf-headers
-
 sed -i "s/autocore-arm/autocore/" target/linux/amlogic/Makefile
 
 wget -N https://github.com/coolsnowwolf/lede/raw/refs/heads/master/include/kernel-6.1 -P target/linux/generic/
